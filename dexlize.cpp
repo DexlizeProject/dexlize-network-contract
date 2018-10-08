374234884ee2d21b5c93bfbf1015eaf9e256312f
apathymagic <apathymagic@163.com>
2018-10-08 18:52:50 +0800

    dexlize new files

---
+++ dexlize.cpp
@@ -0,0 +1,1 @@
+#include "dexlize.hpp"
---
+++ dexlize.hpp
@@ -0,0 +1,15 @@
+/**
+ *  @file
+ *  @copyright defined in BitProtal/LICENSE.txt
+ */
+#pragma once
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+
+namespace proxy {
+    class proxy : public contract {
+        public:
+        proxy() {}
+    }
+}

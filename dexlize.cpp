83c486ac29b8fb069993b71bdc6007e69ede101a
apathymagic <apathymagic@163.com>
2018-10-15 11:27:33 +0800

    add sell action

--- dexlize.cpp
+++ dexlize.cpp
@@ -5,22 +5,72 @@
 #include "dexlize.hpp"
 #include "utils.hpp"
 
-symbol_name Dexlize::Proxy::_string_to_symbol_name(const string& str)
+symbol_name Dexlize::Aux::string2SymbolName(const string& str)
 {
     return string_to_symbol(0, str.c_str()) >> 8;
 }
 
-symbol_name Dexlize::Proxy::_getSymbolName(const string& memo)
+string Dexlize::Aux::_getMemoValue(const string& key, const map<string, string>& memoMap)
 {
-    Utils utils;
-    map<string, string> memoMap = utils.parseJson(memo);
-
-    auto iter = memoMap.find("symbol");
+    auto iter = memoMap.find(key);
     if (iter != memoMap.end()) 
     {
-        return _string_to_symbol_name(iter->second);
+        return iter->second;
     }
-    eosio_assert(iter == memoMap.end(), "invalid memo format for symbol");
+    eosio_assert(iter == memoMap.end(), "invalid memo format");
+}
+
+symbol_name Dexlize::Aux::getSymbolName(const map<string, string>& memoMap)
+{
+    return string2SymbolName(_getMemoValue("symbol", memoMap));
+}
+
+asset Dexlize::Aux::getStakeAmount(const map<string, string>& memoMap)
+{
+    return asset(S(4, _getMemoValue("amount", memoMap)), PUB_SYMBOL);
+}
+
+asset Dexlize::Aux::getEosAmount(const map<string, string>& memoMap)
+{
+    return asset(S(4, _getMemoValue("eos", memoMap)), PUB_SYMBOL);
+}
+
+account_name Dexlize::Aux::getContractAccountName(symbol_name symbolName)
+{
+    account_name contractName;
+    switch (symbolName) 
+    {
+        case PUB_SYMBOL_NAME:
+        contractName = DAP_CONTRACT;
+        break;
+        case TPT_SYMBOL_NAME:
+        contractName = DAP_CONTRACT;
+        break;
+        // TODO: support the kyubey on the stage II
+        case KBY_SYMBOL_NAME:
+        contractName = KBY_CONTRACT;
+        break;
+    }
+    return contractName;
+}
+
+string Dexlize::Aux::getActionMemo(symbol_name symbolName)
+{
+    string memo;
+    switch (symbolName) 
+    {
+        case PUB_SYMBOL_NAME:
+        memo = string("PUB-referrer:").append(GOD_ACCOUNT);
+        break;
+        case TPT_SYMBOL_NAME:
+        memo = string("TPT-referrer:").append(GOD_ACCOUNT);
+        break;
+        // TODO: support the kyubey on the stage II
+        case KBY_SYMBOL_NAME:
+        memo = "";
+        break;
+    }
+    return memo;
 }
 
 /**
@@ -40,40 +90,55 @@ void Dexlize::Proxy::buy(account_name from, account_name to, asset quantity, str
     eosio_assert(quantity.symbol == CORE_SYMBOL, "must pay with CORE token");
 
     // take the symbol name, and the check if the symbol name of PUB/TPT is in the memo
-    symbol_name symbol = _getSymbolName(memo);
+    Utils utils;
+    map<string, string> memoMap = utils.parseJson(memo);
+    symbol_name symbolName = aux.getSymbolName(memoMap);
 
     // get account of contract by the different symbol name in the memo
     // construct the memo of action by the different symbol name in the memo
     // and then execute the transfer action of target contract
-    string actionMemo;
-    account_name contract;
-    switch (symbol) {
-        case PUB_SYMBOL_NAME:
-        contract = DAP_CONTRACT;
-        actiontMemo = string("PUB-referrer:").append(GOD_ACCOUNT);
-        break;
-        case TPT_SYMBOL_NAME:
-        contract = DAP_CONTRACT;
-        actionMemo = string("TPT-referrer:").append(GOD_ACCOUNT);
-        break;
-        case KBY_SYMBOL_NAME:
-        contract = KBY_CONTRACT;
-        actionMemo = "";
-        break;
-    } 
+    account_name contract = aux.getContractAccountName(symbolName);
+    string actionMemo = aux.getActionMemo(symbolName);
     action(permission_level{_self, N(active)},
+        N(eosio.token), N(transfer),
+        make_tuple(_self, contract, quantity, actionMemo)
+    ).send();
+
+    // get the stake quantity and then execute transfer action
+    asset stake = aux.getStakeAmount(memoMap);
+    action(permission_level{_self, N(active)}, 
         contract, N(transfer),
-        make_tuple(_self, from, quantity, actionMemo)
+        make_tuple(_self, from, stake, string("dexlize transfer https://dexlize.io"))
     ).send();
 }
 
-void Dexlize::Proxy::sell() 
+void Dexlize::Proxy::sell(account_name from, account_name to, asset quantity, string memo)
 {
+    require_auth(from);
+    // check the account of from and to
+    
+    // execute the sell action of tokendapppub, transfer quantity from the account of from to dexlize
+    account_name contract = aux.getContractAccountName(quantity.symbol.name);
+    action(permission_level{_self, N(active)},
+        contract,
+        N(sell),
+        make_tuple(_self, contract, quantity, string("dexlize withdraw https://dexlize.io"))
+    ).send();
 
+    // execute the transfer action of eosio.token, transfer the eos from dexlize to the account of from
+    Utils utils;
+    map<string, string> memoMap = utils.parseJson(memo);
+    asset eos = aux.getEosAmount(memoMap);
+    action(permission_level{_self, N(active)},
+        N(eosio.token),
+        N(transfer),
+        make_tuple(_self, from, eos, string("dexlize withdraw https://dexlize.io"))
+    ).send();
 }
 
-void Dexlize::Proxy::convert() 
+void Dexlize::Proxy::convert(account_name from, asset fromAsset, asset toAsset) 
 {
+    require_auth(from);
 
 }
 
@@ -81,15 +146,18 @@ extern "C" {
     void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
         Dexlize::Proxy thiscontract(receiver);
 
-        if((code == N(eosio.token)) && (action == N(transfer))) {
+        if((code == EOSIO_CONTRACT) && (action == N(transfer))) {
             execute_action(&thiscontract, &Dexlize::Proxy::buy);
+            return;
+        } else if ((code == DAP_CONTRACT) && (action == N(transfer))) {
+            execute_action(&thiscontract, &Dexlize::Proxy::sell);
             return;
         }
 
         if (code != receiver) return;
 
         switch (action) {
-            EOSIO_API(Dexlize::Proxy, (version)(buy)(sell))
+            EOSIO_API(Dexlize::Proxy, (version)(sell))
         };
         eosio_exit(0);
     }
--- dexlize.hpp
+++ dexlize.hpp
@@ -12,11 +12,13 @@ using namespace eosio;
 using namespace std;
 
 #define SN(X) (string_to_symbol(0, #X) >> 8)
+#define PUB_SYMBOL S(4, PUB)
 #define PUB_SYMBOL_NAME SN(PUB)
 #define TPT_SYMBOL_NAME SN(TPT)
 #define KBY_SYMBOL_NAME SN(KBY)
 
 #define GOD_ACCOUNT "eostestbp121"
+#define EOSIO_CONTRACT N(eosio.token)
 #define DAP_CONTRACT N(tokendapppub)
 #define KBY_CONTRACT N(myeosgroupon)
 
@@ -26,15 +28,34 @@ namespace Dexlize {
         Proxy(account_name self) : contract(self) {};
         void version();
         void buy(account_name from, account_name to, asset quantity, string memo);
-        void sell();
-        void convert();
+        void sell(account_name from, account_name to, asset quantity, string memo);
+        void convert(account_name from, asset fromAsset, asset toAsset);
 
         private:
-        symbol_name _string_to_symbol_name(const string& str);
-        symbol_name _getSymbolName(const string& memo);
+        Aux aux;
+        // symbol_name _string2SymbolName(const string& str);
+        // string _getMemoValue(const string& key, const map<string, string>& memoMap);
+        // symbol_name _getSymbolName(const map<string, string>& memoMap);
+        // asset _getStakeAmount(const map<string, string>& memoMap);
+        // asset _getEosAmount(const map<string, string>& memoMap);
+        // account_name _getContractAccountName(symbol_name symbolName);
+        // string _getActionMemo(symbol_name symbolName);
+    };
+
+    class Aux {
+        public:
+        symbol_name string2SymbolName(const string& str);
+        symbol_name getSymbolName(const map<string, string>& memoMap);
+        asset getStakeAmount(const map<string, string>& memoMap);
+        asset getEosAmount(const map<string, string>& memoMap);
+        account_name getContractAccountName(symbol_name symbolName);
+        string getActionMemo(symbol_name symbolName);
+
+        private:
+        string _getMemoValue(const string& key, const map<string, string>& memoMap);
     };
 };
 
 #ifdef ABIGEN
-EOSIO_ABI(Dexlize::Proxy, (version)(buy)(sell))
+EOSIO_ABI(Dexlize::Proxy, (version)(sell))
 #endif

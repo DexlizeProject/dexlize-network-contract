36bf8c5d6b32a60c52363bf00a696514ea8d4058
apathymagic <apathymagic@163.com>
2019-03-01 13:51:06 +0800

    fix char value

--- dexlize/include/dexlize.hpp
+++ dexlize/include/dexlize.hpp
@@ -102,6 +102,19 @@ namespace Dexlize {
             return _global.taker_ratio;
         }
 
+        static constexpr uint8_t char_to_value( char c ) {
+            if( c == '.')
+                return 0;
+            else if( c >= '1' && c <= '5' )
+                return (c - '1') + 1;
+            else if( c >= 'a' && c <= 'z' )
+                return (c - 'a') + 6;
+            else
+                eosio_assert( false, "character is not in allowed character set for uuids" );
+
+            return 0; // control flow will never reach here; just added to suppress warning
+        }
+        
         uint64_t stringToInt(const string& str) {
             uint64_t value;
             if( str.size() > 64 ) {
